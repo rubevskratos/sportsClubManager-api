fce7fb04297f7abd87f583c5e81e7da312c13b1b
Ruben Carballo <rcarballoq@gmail.com>
2022-03-18 00:03:30 +0100

    GENERAL
    Started front development

---
+++ frontend/src/components/navBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-toolbar-title>CECC Manager</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn :to="{ name: 'home' }">Home</v-btn>
+    <v-btn :to="{ name: 'about' }">About</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <login-form />
+</template>
+
+<script>
+import LoginForm from "@/components/loginForm";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    LoginForm,
+  },
+};
+</script>
